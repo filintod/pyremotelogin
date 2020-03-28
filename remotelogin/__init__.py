8d982293cf353369703b05aeeece6503082f4287
Filinto Duran <fduran@gaikai.com>
2020-03-28 11:09:52 -0500

    set back import

--- remotelogin/__init__.py
+++ remotelogin/__init__.py
@@ -2,10 +2,7 @@ from .__version__ import __version__
 import logging
 log = logging.getLogger(__name__)
 # need to call settings files to set settings up and register any function
-try:
-    from .devices import settings
-except ModuleNotFoundError:
-    log.warning("Problems importing Device Settings. Disregard is not using devices...")
+from .devices import settings
 from .connections import settings
 
 from fdutils.config import load_default, environment_settings
