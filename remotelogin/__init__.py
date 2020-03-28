321817b59382c7867bd2c46382e0f81a48ff69ab
Filinto Duran <fduran@gaikai.com>
2020-03-28 11:01:07 -0500

    allow for cert to be text

--- remotelogin/__init__.py
+++ remotelogin/__init__.py
@@ -1,7 +1,11 @@
 from .__version__ import __version__
-
+import logging
+log = logging.getLogger(__name__)
 # need to call settings files to set settings up and register any function
-from .devices import settings
+try:
+    from .devices import settings
+except ModuleNotFoundError:
+    log.warning("Problems importing Device Settings. Disregard is not using devices..")
 from .connections import settings
 
 from fdutils.config import load_default, environment_settings
